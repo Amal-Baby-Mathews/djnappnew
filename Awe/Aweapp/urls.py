4f46dd3713e1ed585706840758fcdffa35af0395
Amal-Baby-Mathews <amalbmat2001@gmail.com>
2023-12-19 14:44:53 +0530

    App

--- Awe/Aweapp/urls.py
+++ Awe/Aweapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 from . import   views
 urlpatterns= [
-  path("", views.index, name="index"),  
+  path("<int:id>/", views.index, name="index"), 
+  path("v1/", views.v1, name="v1"), 
+  path("", views.home, name="home"),
+  path("chat/", views.chat, name="chat"),
 ]
--- Awe/Aweapp/views.py
+++ Awe/Aweapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import ToDoList, Item
+from .forms import CreateNewChat
+def home(response):
+    return render(response, "Aweapp/home.html", {"name": "Welcome to Awe!"})
+def index(response, id):
+    ls= ToDoList.objects.get(id=id)
+    return render( response, "Aweapp/index.html", {"name": ls.name , "posts":ls.item_set.all()})
 
-def index(response):
-    return HttpResponse("<h1>Hello, world. You're at the Awe index.</h1>")
+def v1(response):
+    return HttpResponse("<h1>Hello, world. You're at the Awe v1.</h1>")
+
+def chat(response):
+    """Renders the chat page using the Aweapp/chat.html template."""
+    form = CreateNewChat()
+    return render(response, "Aweapp/chat.html", {"form":form})
